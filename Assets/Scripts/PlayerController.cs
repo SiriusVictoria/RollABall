0b7e2e38c4a3c9e2dadc0ddae1fe0e4ec8ad9f24
Sirius Victoria <pelinalvo@gmail.com>
2020-10-20 14:19:59 +0300

    Initilazed Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedMultiplier = 1f;
+
+    private Rigidbody _rigidbody;
+    private float _movementHorizontal, _movementVertical; //aralarındaki virgül daha düzenli olmaları için çünkü aynı anlamı verdik ikisine de
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            _rigidbody.AddForce(x: 0, y: 4f, z: 0, ForceMode.Impulse);
+
+        _movementHorizontal = Input.GetAxis("Horizontal") * speedMultiplier; //*5f hızlandırmak için
+        _movementVertical = Input.GetAxis("Vertical") * speedMultiplier;
+        _rigidbody.AddForce(x: _movementHorizontal, y: 0, z: _movementVertical, ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //collect and destroy coin
+        if (other.transform.CompareTag("Coin"))
+            GameObject.Destroy(other.gameObject);
+    }
+}
