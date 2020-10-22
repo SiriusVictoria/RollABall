fb81a293e4ab9ae99c1fc225b2b8e48bc54974e9
Bator <bator.darzabe2@gmail.com>
2020-10-22 19:49:03 +0300

    Surprise

---
+++ Assets/Scripts/ScrollText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class ScrollText : MonoBehaviour
+{
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+
+    
+
+    
+    void Update()
+    {
+        float offsetX = Time.time * ScrollX;
+        float offsetY = Time.time * ScrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+
+    }
+}
